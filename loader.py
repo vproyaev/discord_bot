7c5932c8d72de9b612dd58737ba847fd979d2233
vproyaev <farmfilok@gmail.com>
2021-11-04 17:13:20 +0300

    update db connect

--- loader.py
+++ loader.py
@@ -1,3 +1,5 @@
+import os
+
 from discord.ext import commands
 from discord import Intents
 
@@ -10,11 +12,7 @@ bot = commands.Bot(command_prefix='.', intents=Intents.all(), help_command=None)
 
 async def create_db_pool():
     bot.db_con = await asyncpg.create_pool(
-        user='cucbziofgbgzbf',
-        password='59276a7be9242b541a4f3931b2112e3a853d77ff0486bfdcfbd0dae2634a0282',
-        host='c2-54-73-110-26.eu-west-1.compute.amazonaws.com',
-        port='5432',
-        database='den8249gj7mdb2'
+        os.environ['DATABASE_URL']
     )
 
 db = DB(bot)
