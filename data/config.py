8f1f9576a3138770069055be97fbd640344c5ad0
vproyaev <farmfilok@gmail.com>
2021-11-04 16:52:49 +0300

    change db settings and add environ token

--- data/config.py
+++ data/config.py
@@ -1,7 +1,9 @@
+import os
+
 from environs import Env
 
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str('BOT_TOKEN')
+BOT_TOKEN = os.environ['BOT_TOKEN']
 CHANNEL_ID = env.str('CHANNEL_ID')
--- loader.py
+++ loader.py
@@ -10,11 +10,11 @@ bot = commands.Bot(command_prefix='.', intents=Intents.all(), help_command=None)
 
 async def create_db_pool():
     bot.db_con = await asyncpg.create_pool(
-        user='tullamore',
-        password='grats',
-        host='127.0.0.1',
+        user='cucbziofgbgzbf',
+        password='59276a7be9242b541a4f3931b2112e3a853d77ff0486bfdcfbd0dae2634a0282',
+        host='c2-54-73-110-26.eu-west-1.compute.amazonaws.com',
         port='5432',
-        database='postgres'
+        database='den8249gj7mdb2'
     )
 
 db = DB(bot)
