6f92fa95fbb344ce3219d128d1c051757ac14b4f
vproyaev <farmfilok@gmail.com>
2021-11-04 16:59:25 +0300

    add URI

--- loader.py
+++ loader.py
@@ -14,7 +14,8 @@ async def create_db_pool():
         password='59276a7be9242b541a4f3931b2112e3a853d77ff0486bfdcfbd0dae2634a0282',
         host='c2-54-73-110-26.eu-west-1.compute.amazonaws.com',
         port='5432',
-        database='den8249gj7mdb2'
+        database='den8249gj7mdb2',
+        URI='postgres://cucbziofgbgzbf:59276a7be9242b541a4f3931b2112e3a853d77ff0486bfdcfbd0dae2634a0282@ec2-54-73-110-26.eu-west-1.compute.amazonaws.com:5432/den8249gj7mdb2'
     )
 
 db = DB(bot)
